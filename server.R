a86f1609febfa56f2ad5c8d67e0fa8e3537601be
Giang Nguyen Rudderham <giang-rudderham@users.noreply.github.com>
2017-04-08 22:56:54 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -11,14 +11,16 @@ library(stringr) # for str_wrap function
 productDPRK <- read.csv(file = "electricity-production-northkorea.csv",
                                   stringsAsFactors = F)
 popDPRK <- read.csv(file = "population-northkorea.csv")
+growthDPRK <- read.csv(file = "growth-electricity-production-dprk.csv")
 
 productSK <- read.csv(file = "electricity-production-southkorea.csv")
 popSK <- read.csv(file = "population-southkorea.csv")
+growthSK <- read.csv(file = "growth-electricity-production-sk.csv")
 
 shinyServer(function(input, output) {
 
   output$barDPRK <- renderPlot({
-    chosen_year <- paste0("X", input$year)
+    chosen_year <- paste0("X", input$yearProduction)
     
     ## change to production (in kWh) per capita
     productDPRK$kwhPerCapita = productDPRK[[chosen_year]] * 1000000 / popDPRK[[chosen_year]]
@@ -37,7 +39,7 @@ shinyServer(function(input, output) {
   })
 
   output$barSK <- renderPlot({
-    chosen_year <- paste0("X", input$year)
+    chosen_year <- paste0("X", input$yearProduction)
     
     ## change to production (in kWh) per capita
     productSK$kwhPerCapita = productSK[[chosen_year]] * 1000000 / popSK[[chosen_year]]
@@ -53,7 +55,42 @@ shinyServer(function(input, output) {
       theme_minimal() +
       scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + #wrap x var names
       ylim(0, 5000) #so that all bar plots have the same y axis
+  })
+  
+  output$growthDPRK <- renderPlot({
+    chosen_year <- paste0("X", input$yearGrowth)
     
+    ## change to growth in kWh per capita
+    growthDPRK$growthPerCapita = growthDPRK[[chosen_year]] * 1000000 / popDPRK[[chosen_year]]
+    ## round to 2 decimal places
+    growthDPRK$growthPerCapita = round(growthDPRK$growthPerCapita, 2)
+    
+    ggplot(data = growthDPRK, aes(x = Fuel, y = growthPerCapita)) +
+      geom_bar(stat = "identity", fill = "steelblue") +
+      geom_text(aes(label = growthDPRK$growthPerCapita), vjust = -0.3, size = 3.5) + #display y values on bars
+      labs(title = "Growth in Electricity Production Per Capita by Fuel in North Korea, 1991-2014", 
+           y = "Growth in Electricity Production (kWh) Per capita") +
+      theme_minimal() +
+      scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + #wrap x var names
+      ylim(-700, 700) #so that all bar plots have the same y axis
+  })
+  
+  output$growthSK <- renderPlot({
+    chosen_year <- paste0("X", input$yearGrowth)
+    
+    ## change to growth in kWh per capita
+    growthSK$growthPerCapita = growthSK[[chosen_year]] * 1000000 / popSK[[chosen_year]]
+    ## round to 2 decimal places
+    growthSK$growthPerCapita = round(growthSK$growthPerCapita, 2)
+    
+    ggplot(data = growthSK, aes(x = Fuel, y = growthPerCapita)) +
+      geom_bar(stat = "identity", fill = "green") +
+      geom_text(aes(label = growthSK$growthPerCapita), vjust = -0.3, size = 3.5) + #display y values on bars
+      labs(title = "Growth in Electricity Production Per Capita by Fuel in South Korea, 1991-2014", 
+           y = "Growth in Electricity Production (kWh) Per capita") +
+      theme_minimal() +
+      scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) + #wrap x var names
+      ylim(-700, 700) #so that all bar plots have the same y axis
   })
   
 })
