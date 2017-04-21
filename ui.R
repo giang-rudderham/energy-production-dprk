c58b751a45e4174215278d4b5a202dc276bdacc4
Giang Nguyen Rudderham <giang-rudderham@users.noreply.github.com>
2017-04-21 17:09:58 -0500

    Increase speed + easier comparison

--- ui.R
+++ ui.R
@@ -1,45 +1,32 @@
 library(shiny)
 library(leaflet)
-
-# Version 3: bar plots of productions of 4 countries
+# V.4: grouped bar plot
+#############################
 shinyUI(navbarPage("Comparison of Energy Production in 4 Countries", id = "nav",
                    
                    tabPanel("Bar Plot",
                             div(class = "outer",
                                 fluidRow(
-                                  column(6,
-                                         plotOutput("barDPRK", height = 275),
-                                         br(),
-                                         br()),
-                                  column(6,
-                                         plotOutput("barSK", height = 275),
-                                         br(),
-                                         br())
+                                  plotOutput("bar")
                                 ) ,
                                 
-                                fluidRow(
-                                  column(6,
-                                         plotOutput("barUS", height = 275),
-                                         br(),
-                                         br()) ,
-                                  column(6,
-                                         plotOutput("barChina", height = 275),
-                                         br(),
-                                         br())
-                                ) ,
+                                hr(),
                                 
                                 fluidRow(
                                   column(3, 
                                          checkboxGroupInput("checkGroup", 
-                                                            label = "Choose countries to display", 
-                                                            choices = list("DPRK" = 1, 
-                                                                          "South Korea" = 2, 
-                                                                          "U.S." = 3,
-                                                                          "China" = 4),
-                                                            selected = 1
-                                                )),
+                                                            label = "Choose countries to display:", 
+                                                            # left side are values to display to users
+                                                            # right side are values sent to input$checkGroup
+                                                            choices = list("North Korea" = "North Korea", 
+                                                                           "China" = "China",
+                                                                           "South Korea" = "South Korea", 
+                                                                           "U.S." = "U.S."
+                                                                           ),
+                                                            selected = "North Korea")
+                                  ),
                                   column(9, 
-                                         sliderInput("yearProduction", "Choose a year:",
+                                         sliderInput("year", "Choose a year:",
                                                      min = 1990, max = 2014, value = 2014,
                                                      animate = TRUE, sep = "")
                                   )
@@ -51,7 +38,7 @@ shinyUI(navbarPage("Comparison of Energy Production in 4 Countries", id = "nav",
                                                  World Development Indicators (data.worldbank.org).",
                                                  "Please send questions and comments to author Giang Rudderham (giang.rudderham@gmail.com)")
                                 )
-                                )
                             )
                    )
+)
 )
